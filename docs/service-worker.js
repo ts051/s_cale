a0aaa43334080b0695fb4b6ad9fc815b36842308
sira3 <masamune.white@gmail.com>
2026-07-03 22:52:47 +0900

    Sync public app assets

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_VERSION = 'whitetree-shell-v44';
+const CACHE_VERSION = 'whitetree-shell-v52';
 const APP_SHELL = [
   './',
   './static/manifest.webmanifest?v=pwa-start-1',
   './static/supabase-config.js?v=account-name-2',
-  './static/local-api.js?v=account-name-2',
+  './static/local-api.js?v=account-name-5',
   './static/fullcalendar-6.1.18.min.js?v=fc-local-1',
   './static/icons/icon-192.png',
   './static/icons/icon-512.png',
